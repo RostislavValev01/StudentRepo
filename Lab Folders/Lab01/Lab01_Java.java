4a71c8d73c95069d2cb41cbf1b707d80d2255afd
Rostislav Valev <220159539@aston.ac.uk>
2024-01-30 10:55:31 +0000

    Removed a comment

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -5,7 +5,6 @@ void testMethod1()
   //First command x = y + z
   //Second commnad print(x)
   // edited by Rostislav Valev 220159539
-  // edited once again by Rostislav Valev 220159539
   //Last line print('Good bye')
 }
 
